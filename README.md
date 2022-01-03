c9bc367fa788b42346a904ef071d5026f83c95ab
jrCleber <cleber.jr@live.com>
2022-01-03 14:41:00 -0300

    Aplication Version 1.0.0

---
+++ README.md
@@ -0,0 +1,20 @@
+## bot-venom-firebase
+
+# Bot de Atendimento Via Whatsapp
+
+Esse projeto foi desenvolvido tendo como base, a bilioteca [orkestral/venom](https://github.com/orkestral/venom) para a manipulação de chats do whatsapp, e utiliza o **Firestore**, banco de dados **NoSql** do **Firebase**.
+
+<hr>
+
+## Objetivo
+Esta aplicação tem como objetivo o atendimento automatizado e o geresnciamento do pedido
+
+### Funções
+1. cadastro de clientes
+2. gerenciamento dos pedidos
+3. cadastro de endereço
+4. cadastro do pedido e envio para a produção
+5. cancelamento do pedido
+<hr>
+
+Essa aplicação não realiza o gerenciamento de pagamentos, roteamentos para atendentes e edições em gerais.
---
+++ app/types/types.ts
@@ -0,0 +1,35 @@
+import { firestore } from "firebase-admin"
+
+export type TRowsMenu = {
+    title: string,
+    description: string
+}[]
+
+export type TButtons = {
+    buttonText: {
+        displayText: string
+    }
+}[]
+
+export type TOrder = {
+    title: string,
+    price: number,
+    description: string,
+    quantity?: number,
+    category?: string,
+}
+
+export type TAddress = {
+    zipCode?: string,
+    city?: string,
+    distryct?: string,
+    publicPlace?: string,
+    number?: string,
+}
+
+export type TDataTemp = {
+    codeState: string,
+    subState: string | firestore.FieldValue,
+    tempAddress: TAddress | firestore.FieldValue,
+    tempOrderList: TOrder[] | firestore.FieldValue
+}
